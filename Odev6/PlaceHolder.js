615b42aa3bf74a910abea9a08ac2ea9866b6fc24
juetschen <60485447+juetschen@users.noreply.github.com>
2020-12-25 23:19:50 +0300

    Odev6

--- Odev6/PlaceHolder.js
+++ Odev6/PlaceHolder.js
@@ -1,79 +1,56 @@
 import React from 'react';
-
 import {FlatList, Text, View, ScrollView,TouchableOpacity} from 'react-native';
-
 export default class PlaceHolder extends React.Component{
-
     constructor(props){
         super(props);
         this.state={
-            users:[],
-            Kullanici:0,
+        e:[],
         }
     }
-
     componentDidMount = async ()=>{
-        let user =  await fetch('https://jsonplaceholder.typicode.com/posts')
+        let aa =  await fetch('https://jsonplaceholder.typicode.com/posts')
         .then(r=>r.json());
         
-            this.setState({users:user})
+            this.setState({e:aa})
     };
-
     handlePress = id =>{
-        this.setState({Kullanici:id})
+        this.setState({y:id})
     }
+    lst(){
+        let render=[];
+        this.state.e.map((v,k)=>{
 
-    userListView(){
-
-        if(this.state.Kullanici<1){
-            return(
-                <Text style={{fontSize:15, margin:17}}>Görmek İstediğiniz Kullanıcı İd'sini Seçiniz</Text>
-            ) 
-        }
-        let renderUserList=[];
-        this.state.users.map((v,k)=>{
-
-            if(v.id===this.state.Kullanici){
-               renderUserList.push(<Text style={{minHeight:75, fontSize:16, paddingLeft:5, marginTop:21}}>
-             <Text style={{color:'magenta', fontSize:16}}>ID:</Text> {v.id} {"\n\n"}
-               <Text style={{color:'magenta', fontSize:16}}>TITLE:</Text> {v.title} {"\n\n"}
-               <Text style={{color:'magenta', fontSize:16}}>BODY:</Text> {v.body} {"\n\n"}</Text>)
+            if(v.id===this.state.y){
+               render.push(
+               <Text style={{minHeight:75, fontSize:16, paddingLeft:5, marginTop:21}}>
+              <Text style={{ fontSize:16,color:'magenta'}}>id:</Text> {v.id}
+               <Text style={{ fontSize:16, color:'magenta'}}>{"\n"}title:</Text>{v.title}
+               <Text style={{ fontSize:16,color:'magenta'}}>{"\n"}body:</Text>{v.body}</Text>
+               )
             }
-
         }
         )
-        return renderUserList
+        return render
     }
-
     render(){
         return(
         <View style={{flex:1}}>
-
-              <Text style={{color: 'white', fontSize:17, textAlign:'center', backgroundColor:'black', padding:7}}>
-                <Text style={{color: 'white', fontSize:17}}>KULLANICI İd'si : </Text>{this.state.Kullanici}</Text>
-
-             <ScrollView style={{flex:1,minHeight:360}}>
-                {this.userListView()}
-                </ScrollView>
-
-            <FlatList style = {{marginTop:10,flex:1,minHeight:257,backgroundColor: 'white'}}
-
-                let data = {this.state.users}
+            <FlatList style = {{marginTop:10,flex:1,minHeight:257,backgroundColor: 'black'}}
+                let data = {this.state.e}
                 renderItem={({item})=>{
-
                     return(
-                       <TouchableOpacity onPress={()=>this.handlePress(item.id)} style={{minHeight: 30, backgroundColor: 'white'}}>
-                           <Text style={{fontSize:18,color:'black', backgroundColor:'cyan',textAlign:'center'}}>{item.id}</Text>
+                       <TouchableOpacity onPress={()=>this.handlePress(item.id)} style={{textAlign:'center', minHeight: 20, backgroundColor: 'white'}}>
+                           <Text style={{textAlign:'center',color:'cyan', backgroundColor:'gray',fontSize:18}}>{item.title}</Text>
                        </TouchableOpacity> 
                     )
                 }
-            }
-            />
-
-                
+                }
+                />
+             <ScrollView style={{flex:1,minHeight:400}}>
+                {this.lst()}
+                </ScrollView>
+            <Text style={{color: 'white', fontSize:17, textAlign:'center', backgroundColor:'black', padding:7}}></Text>
         </View>
-            
-
         )
     }
 }
