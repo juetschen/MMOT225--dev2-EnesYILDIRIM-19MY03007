4c8d01b19afef6dec8317dd76a14f7f6adb4f85f
juetschen <60485447+juetschen@users.noreply.github.com>
2021-01-22 14:02:53 +0300

    Final

---
+++ Final Proje/bulutodev/config.js
@@ -0,0 +1,4 @@
+export const news_url = 'http://newsapi.org/v2/top-headlines';
+export const country_code = 'tr';
+export const category = 'general';
+export const apiKey = 'd8e2ec6c03654f74b1ba619158dd7370';
---
+++ Final Proje/bulutodev/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const FB= firebase.initializeApp(
+ 
+   {
+    apiKey: "AIzaSyCfX2Ae9m7WZX89AyMo4OORPauR_Lownj0",
+    authDomain: "myapp-project-123.firebaseapp.com",
+    databaseURL: "https://finalprojeodevi-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "myapp-project-123",
+    storageBucket: "myapp-project-123.appspot.com",
+    messagingSenderId: "65211879809",
+    appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+    measurementId: "G-8GSGZQ44ST"
+  });
+export default FB;
